4561fcb99d1a70de0b3fdd161cee1890e9e8e5a0
PJ Wickwire <pjpeterww@gmail.com>
2018-05-07 21:11:03 -0400

    Added basic functionality to ranomly fetch dapps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,88 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+import Header from './header';
+import Iframe from 'react-iframe';
+
+const stateOfTheDappsSheetsUrl = "https://sheets.googleapis.com/v4/spreadsheets/1VdRMFENPzjL2V-vZhcc_aa5-ysf243t5vXlxC2b054g/values/A2:D?key=AIzaSyCEHwUcYchfR6-hMtdxYx5GQGFXi4LGFx4"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stateOfTheDapps: [],
+      currentDapp: {
+        name: "Entry",
+        description: "It's dapp dapp goose!",
+        teaser: "tease ya",
+        url: "http://www.youtube.com/embed/xDMP3i36naA",
+      },
+    }
+    this.fetchDappUrl = this.fetchDappUrl.bind(this);
+    this.setCurrentDapp = this.setCurrentDapp.bind(this);
+    this.getRandomInt = this.getRandomInt.bind(this);
+    this.fetchSotd = this.fetchSotd.bind(this);
+  }
+  componentDidMount() {
+    this.fetchSotd();
+  }
+
+  fetchDappUrl() {
+    if (this.state.stateOfTheDapps.length == 0){
+      this.fetchSotd();
+    } else {
+      let currentDapp = this.setCurrentDapp(this.state.stateOfTheDapps.length);
+      this.setState((prevState) => {
+        return {
+          currentDapp: currentDapp
+        }
+      });
+    }
+  }
+
+  fetchSotd() {
+    fetch(`${stateOfTheDappsSheetsUrl}`).then(resp => {
+      return resp.json()
+    }).then( data => {
+      let stateOfTheDapps = [];
+      data.values.forEach(x => {
+        let dappObject = {
+          name: x[0],
+          description: x[1],
+          teaser: x[2],
+          url: x[3]
+        };
+        stateOfTheDapps.push(dappObject);
+      })
+      this.setState((prevState) => {
+        return {
+          stateOfTheDapps: stateOfTheDapps
+        }
+      });
+    })
+  }
+
+  setCurrentDapp(max) {
+    let random = this.getRandomInt(max);
+    return this.state.stateOfTheDapps[random]
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header clickFunction={this.fetchDappUrl}/>
+        <Iframe url={this.state.currentDapp.url}
+                width="100%"
+                height="-webkit-fill-available"
+                id="dapp-container"
+                display="initial"
+                position="relative"
+                allowFullScreen/>
       </div>
     );
   }
